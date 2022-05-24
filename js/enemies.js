8f71342e96577cc225ac7640a1552c9cdbddce2f
MrAlizarinCrimson <64264338+MrAlizarinCrimson@users.noreply.github.com>
2022-05-24 17:15:58 -0400

    Add files via upload

---
+++ js/enemies.js
@@ -0,0 +1,11 @@
+let enemies
+
+function enemies(clase, vida, mana, fuerza, agilidad, velocidad) {
+	this.clase = claseenemiga;
+	this.vida = vida;
+	this.maná = maná;
+	this.fuerza = fuerza;
+	this.agilidad = agilidad;
+	this.velocidad = velocidad;
+
+}
---
+++ js/player.js
@@ -0,0 +1,12 @@
+	let player;
+
+	function player(clase, vida, mana, fuerza, agilidad, velocidad) {
+		this.clase = clase;
+		this.vida = vida;
+		this.maná = maná;
+		this.fuerza = fuerza;
+		this.agilidad = agilidad;
+		this.velocidad = velocidad;
+
+	}
+

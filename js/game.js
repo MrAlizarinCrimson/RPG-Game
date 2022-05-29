39968bdb1e393f465f138458534a12703a45d967
NathalyStudio <andreina3nathaly5@gmail.com>
2022-05-29 10:08:25 -0400

    corrected something

--- js/game.js
+++ js/game.js
@@ -1,84 +1,89 @@
 let gameManager = {
-	setgamestart: function(classtype){
-		this.resetPlayer(classType);
-		this.setpreFight();
-	},
-	resetplayer: function(classtype) {
-		switch (classType) {
-			case "Bulbasaur":
-			player = new player(classtype, 200, 92, 92, 121, 85, 121);
-				break;
+    setgamestart: function(classtype) {
+        this.resetPlayer(classType);
+        this.setpreFight();
+    },
+    resetplayer: function(classtype) {
+        switch (classType) {
+            case "Bulbasaur":
+                player = new player(classtype, 200, 92, 92, 121, 85, 121);
+                break;
 
-			case "Charmander":
-			player = new Player(classtype, 188, 98, 81, 112, 121, 94);
-				break;
+            case "Charmander":
+                player = new Player(classtype, 188, 98, 81, 112, 121, 94);
+                break;
 
-			case "Jigglypuff":
-			player = new player(classtype, 340, 85, 41, 85, 40, 49);
-				break;
+            case "Jigglypuff":
+                player = new player(classtype, 340, 85, 41, 85, 40, 49);
+                break;
 
-			case "Pikachu":
-			player = new player(classtype, 180, 103, 76, 94, 166, 94);
-				break;
+            case "Pikachu":
+                player = new player(classtype, 180, 103, 76, 94, 166, 94);
+                break;
 
-			case "Psyduck":
-			player = new player(classtype, 210, 98, 90, 121, 103, 94);
-				break;
+            case "Psyduck":
+                player = new player(classtype, 210, 98, 90, 121, 103, 94);
+                break;
 
-			case "Snorlax":
-			player = new player(classtype, 430, 202, 121, 121, 58, 202);
-				break;
-
-					
-		}
-		
-		let getInterface = document.querySelector(".interface");
-		getInterface.innerHTML = '<img src="img/avatar-player/' + classType.tolowerCase() + '.png" class="img-avatar"><div><h3>' + classType + '</h3><p class="vida-player">Vida: ' + player.vida + '</p><p>Ataque: ' + player.Ataque + ' </p><p>Defensa: ' + player.Defensa + '</p><p>AtaqueSp: ' + player.AtaqueSp + '</p><p>Velocidad: ' + player.Velocidad + '</p><p>DefensaSp: ' + player.DefensaSp + '</p></div>';
-
-	},
-	setprefight: function() {
-		let getHeader = document.querySelector(".header");
-		let getActions = document.querySelector(".actions");
-		let getArena = document.querySelector(".arena");
-
-		getHeader.innerHTML = '<p>Inicio de Batalla</p>';
-		getActions.innerHTML = '<a href="#' class="btn-prefight" onclick="gameManager.setFight()">Selecciona un contricante</a>';
-		getArena.style.visibility = "visible";
-	},
+            case "Snorlax":
+                player = new player(classtype, 430, 202, 121, 121, 58, 202);
+                break;
 
 
-	setFight: function() {
-		let getHeader = document.querySelector(".header");
-		let getActions = document.querySelector(".actions");
-		let getEnemy = document.querySelector(".enemy");
+        }
 
-		crear enemigo
+        let getInterface = document.querySelector(".interface");
+        getInterface.innerHTML = '<img src="img/avatar-player/' + classType.tolowerCase() + '.png" class="img-avatar"><div><h3>' + classType + '</h3><p class="vida-player">Vida: ' + player.vida + '</p><p>Ataque: ' + player.Ataque + ' </p><p>Defensa: ' + player.Defensa + '</p><p>AtaqueSp: ' + player.AtaqueSp + '</p><p>Velocidad: ' + player.Velocidad + '</p><p>DefensaSp: ' + player.DefensaSp + '</p></div>';
 
-		let enemy00 = new Enemy("Evee", 200, 103, 94, 85, 103, 121)
-		let enemy01 = new Enemy("Caterpie", 200, 58, 67, 40, 85, 40)
-		let enemy02 = new Enemy("Venonat", 230, 103, 94, 76, 85, 103)
-		let enemy03 = new Enemy("Dratini", 192, 119, 85, 94, 94, 94)
-		let enemy04 = new Enemy("Meowth", 190, 85, 67, 76, 166, 76)
-		let enemy05 = new Enemy("Squirtle", 198, 90, 121, 94, 81, 119)
+    },
+    setprefight: function() {
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getArena = document.querySelector(".arena");
 
-		//pensaba en quizá el enemigo sea aleatorio... pero creo mejor no a estas alturas hahahah
-		
-		let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2));
-		console.log(chooseRandomEnemy);
-		switch(chooseRandomEnemy) {
-			case 0:
-			enemy = enemy00;
-			break;
-			case 1:
-			enemy = enemy01
-			break;
-		}
-
-		getHeader.innerHTML = '<p>Elige tu movimiento</p>'
-		getActions.innerHTML = '<a href="#' class="btn-prefight" onclick="PlayerMoves.calcAttack()">Ataca!</a>';
-		getEnemy.innerHTML = '<img src="img/avatar-enemies/' + enemy.enemyType.tolowerCase() + '.png" alt="' + enemy.enemyType + '" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="vida-enemy">Vida: ' + enemy.vida + '</h3><p>Ataque: ' + enemy.Ataque + '</p><p>'Defensa: ' + enemy.Defensa + '</p><p>AtaqueSp: ' + enemy.AtaqueSp + '</p><p>Velocidad: ' + enemy.Velocidad + '</p><p>DefensaSp: ' + enemy.DefensaSp + '</p></div>';
+        getHeader.innerHTML = '<p>Inicio de Batalla</p>';
+        getActions.innerHTML = '<a href="#'
+            /*  class = "btn-prefight"
+             onclick = "gameManager.setFight()" > Selecciona un contricante < /a>';
+             getArena.style.visibility = "visible"; */
+    },
 
 
+    setFight: function() {
+        let getHeader = document.querySelector(".header");
+        let getActions = document.querySelector(".actions");
+        let getEnemy = document.querySelector(".enemy");
 
-	}
+        /*  crear enemigo
+         */
+        let enemy00 = new Enemy("Evee", 200, 103, 94, 85, 103, 121)
+        let enemy01 = new Enemy("Caterpie", 200, 58, 67, 40, 85, 40)
+        let enemy02 = new Enemy("Venonat", 230, 103, 94, 76, 85, 103)
+        let enemy03 = new Enemy("Dratini", 192, 119, 85, 94, 94, 94)
+        let enemy04 = new Enemy("Meowth", 190, 85, 67, 76, 166, 76)
+        let enemy05 = new Enemy("Squirtle", 198, 90, 121, 94, 81, 119)
+
+        //pensaba en quizá el enemigo sea aleatorio... pero creo mejor no a estas alturas hahahah
+
+        let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(2));
+        console.log(chooseRandomEnemy);
+        switch (chooseRandomEnemy) {
+            case 0:
+                enemy = enemy00;
+                break;
+            case 1:
+                enemy = enemy01
+                break;
+        }
+
+        getHeader.innerHTML = '<p>Elige tu movimiento</p>'
+        getActions.innerHTML = '<a href="#'
+            /*   class = "btn-prefight"
+        onclick = "PlayerMoves.calcAttack()" > Ataca! < /a>';
+        getEnemy.innerHTML = '<img src="img/avatar-enemies/' + enemy.enemyType.tolowerCase() + '.png" alt="' + enemy.enemyType + '" class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="vida-enemy">Vida: ' + enemy.vida + '</h3><p>Ataque: ' + enemy.Ataque + '</p><p>'
+        Defensa: ' + enemy.Defensa + ' < /p><p>AtaqueSp: ' + enemy.AtaqueSp + '</p > < p > Velocidad: ' + enemy.Velocidad + ' < /p><p>DefensaSp: ' + enemy.DefensaSp + '</p > < /div>';
+ */
+
+
+    }
 }
--- js/player.js
+++ js/player.js
@@ -1,110 +1,113 @@
 	let player;
 
 	function Player(clase, Vida, ataque, defensa, ataquesp, velocidad, defensasp) {
-		this.clase = clase;
-		this.vida = vida;
-		this.ataque = ataque;
-		this.defensa = defensa;
-		this.ataquesp = ataquesp;
-		this.velocidad = velocidad;
-		this.defensasp = defensasp;
+	    this.clase = clase;
+	    this.vida = vida;
+	    this.ataque = ataque;
+	    this.defensa = defensa;
+	    this.ataquesp = ataquesp;
+	    this.velocidad = velocidad;
+	    this.defensasp = defensasp;
 
 	}
 
 	let PlayerMoves = {
-		calcAttack: function() {
-			//quien ataca primero?
-			let getPlayerVelocidad = player.velocidad;
-			let getEnemyVelocidad = enemy.velocidad;
-			//Player Attack
-			let player.Attack = function() {´
-				let calcBaseDamage;
-				if (player.defensa > 0) {
-					calcBaseDamage = player.ataque * player.vida / 1000;
+	    calcAttack: function() {
+	        //quien ataca primero?
+	        let getPlayerVelocidad = player.velocidad;
+	        let getEnemyVelocidad = enemy.velocidad;
+	        //Player Attack
+	        let player = function() {
+	                let calcBaseDamage;
+	                if (player.defensa > 0) {
+	                    calcBaseDamage = player.ataque * player.vida / 1000;
 
-				} else {
-					calcBaseDamage = player.ataque * player.defensa / 1000;
+	                } else {
+	                    calcBaseDamage = player.ataque * player.defensa / 1000;
 
-				}
-				let offsetDamage = math.floor(Math.random() * Math.floor(10));
-				let calcOutputDamage = calcBaseDamage + offsetDamage;
-				//Cantidad de golpes
-				let numberOfHits = Math.floor(Math.random() * Math.floor(player.velocidad/ 10) / 2) + 1;
-				let attackValues = [calcOutputDamage, numberOfHits];
-				return attackValues;
+	                }
+	                let offsetDamage = math.floor(Math.random() * Math.floor(10));
+	                let calcOutputDamage = calcBaseDamage + offsetDamage;
+	                //Cantidad de golpes
+	                let numberOfHits = Math.floor(Math.random() * Math.floor(player.velocidad / 10) / 2) + 1;
+	                let attackValues = [calcOutputDamage, numberOfHits];
+	                return attackValues;
 
-				}
-				// Enemy Attack
-				let enemy.Attack = function() {´
-				let calcBaseDamage;
-				if (enemy.vida > 0) {
-					calcBaseDamage = enemy.strength * enemy.vida / 1000;
+	            }
+	            // Enemy Attack
+	        let enemy = function() {
+	                let calcBaseDamage;
+	                if (enemy.vida > 0) {
+	                    calcBaseDamage = enemy.strength * enemy.vida / 1000;
 
-				} else {
-					calcBaseDamage = enemy.strength * enemy.velocidad/ 1000;
+	                } else {
+	                    calcBaseDamage = enemy.strength * enemy.velocidad / 1000;
 
-				}
-				let offsetDamage = math.floor(Math.random() * Math.floor(10));
-				let calcOutputDamage = calcBaseDamage + offsetDamage;
-				//Cantidad de golpes
-				let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.velocidad / 10) / 2) + 1;
-				let attackValues = [calcOutputDamage, numberOfHits];
-				return attackValues;
-			}
-			//get player/enemy vida para que cambie luego!
-			let getPlayerVida = document.querySelector(".vida-player");
-			let getEnemyVida = document.querySelector(".vida-enemy");
-			//Inicio de ataque
-			if(getPlayerVelocidad >= getEnemyVelocidad) {
-				let playerAttackValues = playerAttack();
-				let totaldDamage = playerAttackValues[0] * playerAttackValues[1];
-				enemy.vida = enemy.vida - totaldDamage;
-				alert("Inflingiste" + playerAttackValues[0] + "de daño" + playerAttackValues[1] + "veces.");
-				if (enemy.vida<=0) {
-					alert("¡Has ganado! Actualiza el navegador para jugar nuevamente."); 
-					getPlayerVida.innerHTML ='Vida': ' + player.vida;
-					getEnemyVida.innerHTML ='Vida': 0';
-				} else {
-					getEnemyVida.innerHTML ='Vida': ' + enemy.vida;
+	                }
+	                let offsetDamage = math.floor(Math.random() * Math.floor(10));
+	                let calcOutputDamage = calcBaseDamage + offsetDamage;
+	                //Cantidad de golpes
+	                let numberOfHits = Math.floor(Math.random() * Math.floor(enemy.velocidad / 10) / 2) + 1;
+	                let attackValues = [calcOutputDamage, numberOfHits];
+	                return attackValues;
+	            }
+	            //get player/enemy vida para que cambie luego!
+	        let getPlayerVida = document.querySelector(".vida-player");
+	        let getEnemyVida = document.querySelector(".vida-enemy");
+	        //Inicio de ataque
+	        if (getPlayerVelocidad >= getEnemyVelocidad) {
+	            let playerAttackValues = playerAttack();
+	            let totaldDamage = playerAttackValues[0] * playerAttackValues[1];
+	            enemy.vida = enemy.vida - totaldDamage;
+	            alert("Inflingiste" + playerAttackValues[0] + "de daño" + playerAttackValues[1] + "veces.");
+	            if (enemy.vida <= 0) {
+	                alert("¡Has ganado! Actualiza el navegador para jugar nuevamente.");
+	                getPlayerVida.innerHTML = 'Vida: ' + player.vida;
+	                getEnemyVida.innerHTML = 'Vida: 0 ';
+	            } else {
+	                getEnemyVida.innerHTML = 'Vida ' + '$enemy.vida ';
+	            }
+	            //Ataque enemigo
+	            let enemyAttackValues = enemyAttack();
+	            let totaleDamage = enemyAttackValues[0] * enemyAttackValues[1];
+	            player.vida = player.vida - totaleDamage;
+	            alert("Golpe enemigo" + enemyAttackValues[0] + "de daño" + enemyAttackValues[1] + "veces.");
+	            if (player.vida <= 0) {
+	                alert("¡Has perdido! Actualiza el navegador para jugar nuevamente.");
+	                getPlayerVida.innerHTML = 'Vida: 0 ';
+	                getEnemyVida.innerHTML = 'Vida:' + enemy.vida;
+	            } else {
+	                getPlayerVida.innerHTML = 'Vida: ' + player.vida;
+	            }
+	        } else if (getEnemyVelocidad >= getPlayerVelocidad) {
+	            let enemyAttackValues = enemyAttack();
+	            let totaldDamage = enemyAttackValues[0] * enemyAttackValues[1];
+	            player.vida = player.vida - totaldDamage;
+	            alert("Inflingiste" + enemyAttackValues[0] + "de daño" + enemyAttackValues[1] + "veces.");
+	            if (player.vida <= 0) {
+	                alert("¡Has perdido! Actualiza el navegador para jugar nuevamente.");
+	                getEnemyVida.innerHTML = 'Vida:' + enemy.vida;
+	                getPlayerVida.innerHTML = 'Vida:' + 0;
+	            } else {
+	                getPlayerVida.innerHTML = 'Vida: ' + player.vida;
 
-			//Ataque enemigo
-			let enemyAttackValues = enemyAttack();
-			let totaldDamage = enemyAttackValues[0] * enemyAttackValues[1];
-				player.vida = player.vida - totaldDamage;
-				alert("Golpe enemigo" + enemyAttackValues[0] + "de daño" + enemyAttackValues[1] + "veces.");
-				if (player.vida<=0) {
-					alert("¡Has perdido! Actualiza el navegador para jugar nuevamente."); 
-					getPlayerVida.innerHTML ='Vida': 0 ';
-					getEnemyVida.innerHTML ='Vida': ' + enemy.vida;
-				} else {
-					getPlayerVida.innerHTML = 'Vida: ' + player.vida;
-				}
+	                //Ataque player
+	                let playerAttackValues = playerAttack();
+	                let totaldDamage = playerAttackValues[0] * playerAttackValues[1];
+	                enemy.vida = enemy.vida - totaldDamage;
+	                alert("Golpeaste" + playerAttackValues[0] + "de daño" + playerAttackValues[1] + "veces.");
+	                if (enemy.vida <= 0) {
+	                    alert("¡Has ganado! Actualiza el navegador para jugar nuevamente.");
+	                    getEnemyVida.innerHTML = 'Vida: 0 ';
+	                    getPlayerVida.innerHTML = 'Vida: ' + player.vida;
+	                } else {
+	                    getEnemyVida.innerHTML = 'Vida: ' + enemy.vida;
 
+	                }
 
-			} else if(getEnemyVelocidad >= getPlayerVelocidad) {
-				let enemyAttackValues = enemyAttack();
-				let totaldDamage = enemyAttackValues[0] * enemyAttackValues[1];
-				player.vida = player.vida - totaldDamage;
-				alert("Inflingiste" + enemyAttackValues[0] + "de daño" + enemyAttackValues[1] + "veces.");
-				if (player.vida<=0) {
-					alert("¡Has perdido! Actualiza el navegador para jugar nuevamente."); 
-					getEnemyVida.innerHTML ='Vida': ' + enemy.vida;
-					getPlayerVida.innerHTML ='Vida': 0';
-				} else {
-					getPlayerVida.innerHTML ='Vida': ' + player.vida;
+	            }
 
-			//Ataque player
-			let playerAttackValues = playerAttack();
-			let totaldDamage = playerAttackValues[0] * playerAttackValues[1];
-				enemy.vida = enemy.vida - totaldDamage;
-				alert("Golpeaste" + playerAttackValues[0] + "de daño" + playerAttackValues[1] + "veces.");
-				if (enemy.vida<=0) {
-					alert("¡Has ganado! Actualiza el navegador para jugar nuevamente."); 
-					getEnemyVida.innerHTML ='Vida': 0 ';
-					getPlayerVida.innerHTML ='Vida': ' + player.vida;
-				} else {
-					getEnemyVida.innerHTML = 'Vida: ' + enemy.vida;
-		
-		}
+	        }
+	    }
 
-		}
+	}
